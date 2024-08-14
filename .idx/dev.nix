867cce74f9a5e5d8110b09436ef5c90d45978938
lingkai <lingkai@google.com>
2024-08-14 14:39:11 +0000

    Move app code to root

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,9 +1,62 @@
-# This nix config is only for building the template itself
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "unstable";
-  # Enable the IDX Template CLI
+  # Which nixpkgs channel to use.
+  channel = "unstable"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.python3
+    pkgs.firebase-tools
+    pkgs.terraform
+    pkgs.nodejs
   ];
-  idx.internal.templates-cli.enable = true;
+
+  # Sets environment variables in the workspace
+  env = {
+    GOOGLE_PROJECT = "<project-id>";
+    CLOUDSDK_CORE_PROJECT = "<project-id>";
+    TF_VAR_project = "<project-id>";
+    NG_CLI_ANALYTICS="false";
+  };
+
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "hashicorp.terraform"
+      "angular.ng-template"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [
+            "ng"
+            "serve"
+            "--port"
+            "$PORT"
+          ];
+
+          manager = "web";
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        terraform = ''
+          terraform init --upgrade
+          terraform apply --auto-approve
+        '';
+        npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        npm-start = "npm start";
+      };
+    };
+  };
 }
